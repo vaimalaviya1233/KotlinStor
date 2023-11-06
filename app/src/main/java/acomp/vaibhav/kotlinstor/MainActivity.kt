510707b138080a108748b7f5cbfcc8bd9ceb251e
vaimalaviya1233 <55515799+vaimalaviya1233@users.noreply.github.com>
2023-11-06 17:52:12 -0600

    Page 19

---
+++ app/src/main/java/acomp/vaibhav/kotlinstor/MainActivity.kt
@@ -0,0 +1,15 @@
+package acomp.vaibhav.kotlinstor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.URL
+import kotlin.io.*;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
